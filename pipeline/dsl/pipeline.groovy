de4b779a6af067f6389124b3667637e012e34f77
Sean Leavey <sean.leavey@stelligent.com>
2016-05-19 20:57:54 -0500

    inital commit

---
+++ pipeline/dsl/pipeline.groovy
@@ -0,0 +1,21 @@
+pipeline_repo = "stelligent/pipeline_sec_group.git"
+
+workflowJob('security-pipeline') {
+  triggers {
+    scm("* * * * *")
+  }
+  multiscm {
+    definition {
+      cpsScm {
+        scm {
+            git {
+              remote {
+                  github(pipeline_repo)
+              }
+            }
+        }
+      scriptPath('pipeline/workflow/security_group_pipeline.groovy')
+      }
+    }
+  }
+}
---
+++ pipeline/workflow/security_group_pipeline.groovy
@@ -0,0 +1,34 @@
+
+node('master') {
+  deleteDir()
+  stage("Commit")
+    try {
+      sh("echo hello")
+    } catch(err) {
+      sh("echo goodbye")
+    }
+  stage("Acceptance")
+    try {
+      sh("echo hello1")
+    } catch(err) {
+      sh("echo goodbye1")
+    }
+  stage("Capacity")
+    try {
+      sh("echo hello2")
+    } catch(err) {
+      sh("echo goodbye2")
+    }
+  stage("Exploratory")
+    try {
+      sh("echo hello3")
+    } catch(err) {
+      sh("echo goodbye3")
+    }
+  stage("Production")
+    try {
+      sh("echo hello4")
+    } catch(err) {
+      sh("echo goodbye4")
+    }
+}
