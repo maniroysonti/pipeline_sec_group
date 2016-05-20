78a79f6b43289f2d44475202e4cb98bc64bda229
Sean Leavey <sean.leavey@stelligent.com>
2016-05-19 21:29:07 -0500

    added rvm ruby

--- pipeline/workflow/security_group_pipeline.groovy
+++ pipeline/workflow/security_group_pipeline.groovy
@@ -3,6 +3,7 @@ node('master') {
   deleteDir()
   stage("Commit")
     try {
+      sh("gem install bundler")
       sh("bundle update")
       sh("gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3")
       sh("sudo \\curl -sSL https://get.rvm.io | bash -s stable --ruby")
